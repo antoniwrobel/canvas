ac70c842f314a7ffffec313695c5fcde104e1a05
anton <antoni.wrobel@senstill.pl>
2020-09-08 16:08:14 +0200

    remove unused vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useCallback, useMemo } from "react"
+import React, { useRef, useEffect, useState, useCallback } from "react"
 import "./App.css"
 
 import * as S from "./styles"
